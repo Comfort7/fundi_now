ac9898e734a9eaf8f2b8ed93069073dc72711402
Comfort7 <comfortehachi07@gmail.com>
2025-08-09 12:20:19 +0300

    fix errors

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client';
 
 import React, { useState } from 'react';
@@ -325,7 +326,7 @@ export default function AuthPage() {
           {/* Footer */}
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600">
-              {isLogin ? <>Don{'\'t'} have an account? </> : <>Already have an account? </>}
+              {isLogin ? `Don't have an account? ` : `Already have an account? `}
               <button
                 onClick={() => setIsLogin(!isLogin)}
                 className="text-blue-600 hover:text-blue-500 font-medium"
